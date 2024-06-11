d53b3a5524efbb9506157ce3e8c019127c89f8a9
Josh Brown <josh@brownj.org>
2024-06-11 17:48:16 -0500

    allow escape to clear all toasts

--- src/components/ToastProvider/ToastProvider.js
+++ src/components/ToastProvider/ToastProvider.js
@@ -5,6 +5,11 @@ export const ToastProviderContext = React.createContext();
 function ToastProvider({ children }) {
   const [toastList, setToastList] = React.useState([]);
 
+  // function to clear all toasts
+  function clearAllToasts() {
+    setToastList([]);
+  }
+
   function removeToastWithId(id) {
     const nextToastList = toastList.filter((toast) => toast.id !== id);
     setToastList(nextToastList);
@@ -21,6 +26,34 @@ function ToastProvider({ children }) {
 
     setToastList(nextToastList);
   }
+
+  // // remove the last toast after 5 seconds
+  // React.useEffect(() => {
+  //   const lastToast = toastList[toastList.length - 1];
+  //   if (lastToast) {
+  //     const timeoutId = setTimeout(() => {
+  //       removeToastWithId(lastToast.id);
+  //     }, 5000);
+
+  //     return () => {
+  //       clearTimeout(timeoutId);
+  //     };
+  //   }
+  // }, [toastList]);
+
+  React.useEffect(() => {
+    function handleKeyDown(event) {
+      if (event.code === "Escape") {
+        clearAllToasts();
+      }
+    }
+
+    window.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  }, []);
 
   return (
     <ToastProviderContext.Provider
